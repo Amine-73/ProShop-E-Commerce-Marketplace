79c1df32daa500545e65aaa7aa48aa5f4b66ac99
Amine <aminechanna@gmail.com>
2026-01-04 02:05:22 +0100

    feat:add Hero section and Header and working in responsive product grid

---
+++ client/app/Components/Products/Products.js
@@ -0,0 +1,26 @@
+const Products = [
+  {
+    _id: '1',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '', // Place this image in your /public folder
+    description: 'Bluetooth technology lets you connect it with compatible devices wirelessly',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'iPhone 13 Pro 256GB Memory',
+    image: '',
+    description: 'Introducing the iPhone 13 Pro. A dramatically more powerful camera system.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    rating: 4.0,
+    numReviews: 8,
+  }
+];
+
+export default Products;
