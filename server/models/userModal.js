3ffb1400273b78fd4f3cd5a27c2cb1d0039a85e8
Amine-73 <aminchana.business@gmail.com>
2026-01-24 00:01:13 +0100

    feat:fix ESM module resolution ,add user controllers and createuser Router

--- server/models/userModal.js
+++ server/models/userModal.js
@@ -12,5 +12,20 @@ const userShema=mongoose.Schema(
     }
 )
 
+// Method to compare entered password with hashed password in db 
+userShema.methods.matchPassword=async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+};
+
+//Middleware to hash password before saving to db
+userShema.pre('save',async function (next){
+    if(!this .isModified('password')){
+        next();
+    }
+    const salt=await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password,salt);
+})
+
+
 const User=mongoose.model('User',userShema);
 export default User;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from './routes/userRoutes.js';
 //load envirenment variables
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use('/api/users',userRoutes)
 
 app.get("/", (req, res) => {
   res.send("API is running ... ");
